06ba43a37eabab948bb72c928c8c696b81cc6e7e
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-29 12:38:23 +0100

    fix(api): correct null pointer exception handling in CreateNewUserUseCase

---
+++ api/src/main/java/com/mailboom/api/application/usecase/CreateNewUserUseCase.java
@@ -0,0 +1,29 @@
+package com.mailboom.api.application.usecase;
+
+import com.mailboom.api.domain.entity.User;
+import com.mailboom.api.domain.entity.valueobjects.*;
+import com.mailboom.api.domain.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@AllArgsConstructor
+public class CreateNewUserUseCase {
+    private final UserRepository userRepository;
+
+
+    public User execute(String email, String password, PlanType plan) {
+        if (userRepository.existsByEmail(email)) {
+            throw new IllegalArgumentException("User with email " + email + " already exists");
+        }
+
+        UserId userId = UserId.generate();
+        Email userEmail = Email.fromString(email);
+        PasswordHash passwordHash = PasswordHash.fromString(password);
+
+        User newUser = User.create(userId, userEmail, passwordHash, plan, EmailCounter.zero());
+        return userRepository.save(newUser);
+    }
+}
