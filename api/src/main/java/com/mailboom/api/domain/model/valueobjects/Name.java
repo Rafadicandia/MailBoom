fe81377edba016d09e89a129ba1ac0b35a9d16d6
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-01 13:18:56 +0100

    feat: add Name Value Object tovalidate User data

---
+++ api/src/main/java/com/mailboom/api/domain/model/valueobjects/Name.java
@@ -0,0 +1,23 @@
+package com.mailboom.api.domain.model.valueobjects;
+
+public record Name(String name) {
+    public Name {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        if (name.length() >
+                255) {
+            throw new IllegalArgumentException("Name cannot be longer than 255 characters");
+        }
+
+    }
+
+    public static Name fromString(String name) {
+        return new Name(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
