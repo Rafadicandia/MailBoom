3d156748a62196da0dde1e16fcc25e6a6daffd15
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-02 12:13:20 +0100

    feat(model): add listId to Contact model and update related methods

--- api/src/main/java/com/mailboom/api/domain/model/Contact.java
+++ api/src/main/java/com/mailboom/api/domain/model/Contact.java
@@ -1,6 +1,7 @@
 package com.mailboom.api.domain.model;
 
 import com.mailboom.api.domain.model.valueobjects.ContactId;
+import com.mailboom.api.domain.model.valueobjects.ContactListId;
 import com.mailboom.api.domain.model.valueobjects.Email;
 import com.mailboom.api.domain.model.valueobjects.Name;
 import lombok.Getter;
@@ -11,29 +12,31 @@ import java.util.Map;
 public class Contact {
 
     private final ContactId id;
+    private final ContactListId listId;
     private final  Email email;
     private final Name name;
     private final Map<String, Object> customFields;
     private final boolean subscribed;
 
-    private Contact(ContactId id, Email email, Name name, Map<String, Object> customFields, boolean subscribed) {
+    private Contact(ContactId id, ContactListId listId, Email email, Name name, Map<String, Object> customFields, boolean subscribed) {
         this.id = id;
+        this.listId = listId;
         this.email = email;
         this.name = name;
         this.customFields = customFields;
         this.subscribed = subscribed;
     }
 
-    public static Contact create(ContactId id, Email email, Name name, Map<String, Object> customFields, boolean subscribed){
-        return new Contact(id, email, name, customFields, subscribed);
+    public static Contact create(ContactId id, ContactListId listId, Email email, Name name, Map<String, Object> customFields, boolean subscribed){
+        return new Contact(id, listId, email, name, customFields, subscribed);
     }
 
     public Contact subscribe(Contact contact){
-        return Contact.create(id, email, name, customFields, true);
+        return Contact.create(id, listId, email, name, customFields, true);
     }
 
     public Contact unSubscribe(Contact contact){
-        return Contact.create(id, email, name, customFields, false);
+        return Contact.create(id, listId, email, name, customFields, false);
     }
 
 
