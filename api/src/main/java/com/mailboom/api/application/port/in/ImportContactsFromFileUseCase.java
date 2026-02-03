a5c8aeb2bae8ff9231f97cafb20a6318388d2898
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-03 10:15:39 +0100

    refactor(api): update ImportContactsFromFileUseCase to align with new model structure

---
+++ api/src/main/java/com/mailboom/api/application/port/in/ImportContactsFromFileUseCase.java
@@ -0,0 +1,10 @@
+package com.mailboom.api.application.port.in;
+
+import com.mailboom.api.domain.model.valueobjects.ContactListId;
+import com.mailboom.api.domain.model.valueobjects.UserId;
+
+import java.io.InputStream;
+
+public interface ImportContactsFromFileUseCase {
+    void execute(ContactListId listId, UserId ownerId, InputStream fileStream, String contentType);
+}
