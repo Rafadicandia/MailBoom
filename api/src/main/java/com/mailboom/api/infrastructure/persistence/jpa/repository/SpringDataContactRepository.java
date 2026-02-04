1a2113e332e551cd0ce3c54b25083a44f4a62ac1
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-04 11:28:09 +0100

    feat(api): add method to find contacts by contact list ID

--- api/src/main/java/com/mailboom/api/infrastructure/persistence/jpa/repository/SpringDataContactRepository.java
+++ api/src/main/java/com/mailboom/api/infrastructure/persistence/jpa/repository/SpringDataContactRepository.java
@@ -3,7 +3,9 @@ package com.mailboom.api.infrastructure.persistence.jpa.repository;
 import com.mailboom.api.infrastructure.persistence.jpa.entity.ContactEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SpringDataContactRepository extends JpaRepository<ContactEntity, UUID> {
+    List<ContactEntity> findAllByContactListId_Id(UUID contactListId);
 }
