78bfddcb9f0086d0e23504e5dcef4fa5ac1f222a
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-05 10:05:11 +0100

    feat(api): add method to find contact by email

--- api/src/main/java/com/mailboom/api/infrastructure/persistence/jpa/repository/SpringDataContactRepository.java
+++ api/src/main/java/com/mailboom/api/infrastructure/persistence/jpa/repository/SpringDataContactRepository.java
@@ -4,8 +4,10 @@ import com.mailboom.api.infrastructure.persistence.jpa.entity.ContactEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SpringDataContactRepository extends JpaRepository<ContactEntity, UUID> {
     List<ContactEntity> findAllByContactListId_Id(UUID contactListId);
+    Optional<ContactEntity> findByEmail(String email);
 }
