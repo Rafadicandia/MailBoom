626039882843566410ce39e14a9c1b31ca419ef6
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-05 10:05:00 +0100

    fix(api): correct import path for UserWithEmailAlreadyExistsException

--- api/src/main/java/com/mailboom/api/application/common/exception/UserWithEmailAlreadyExistsException.java
+++ api/src/main/java/com/mailboom/api/application/common/exception/UserWithEmailAlreadyExistsException.java
@@ -1,4 +1,4 @@
-package com.mailboom.api.application.exception;
+package com.mailboom.api.application.common.exception;
 
 public class UserWithEmailAlreadyExistsException extends RuntimeException {
     public UserWithEmailAlreadyExistsException(String message) {
