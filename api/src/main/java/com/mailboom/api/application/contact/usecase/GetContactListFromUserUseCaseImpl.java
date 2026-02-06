8455846d28471940d5a8bb7f6a46b76c37ea3262
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-06 16:04:04 +0100

    feat: add GetContactListFromUserUseCaseImpl

---
+++ api/src/main/java/com/mailboom/api/application/contact/usecase/GetContactListFromUserUseCaseImpl.java
@@ -0,0 +1,32 @@
+package com.mailboom.api.application.contact.usecase;
+
+import com.mailboom.api.application.contact.port.in.GetContactListFromUserUseCase;
+import com.mailboom.api.application.contact.usecase.command.GetContactListFromUserCommand;
+import com.mailboom.api.domain.model.contact.ContactList;
+import com.mailboom.api.domain.model.user.valueobjects.UserId;
+import com.mailboom.api.domain.port.out.ContactListRepository;
+import com.mailboom.api.domain.port.out.UserRepository;
+import com.mailboom.api.infrastructure.exception.UserNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@AllArgsConstructor
+public class GetContactListFromUserUseCaseImpl implements GetContactListFromUserUseCase {
+
+    private final ContactListRepository contactListRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public List<ContactList> execute(GetContactListFromUserCommand command) {
+        UserId userId = new UserId(UUID.fromString(command.ownerId()));
+        if (userRepository.findById(userId) == null) {
+            throw new UserNotFoundException("User not found");
+        }
+
+        return contactListRepository.findAllByUserId(userId);
+    }
+}
