0168b65d2a00815e250c03d85bba385c0cd8278f
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-07 12:17:51 +0100

    refactor(api): make Campaign class immutable by converting mutable fields to final and updating methods to return new instances

--- api/src/main/java/com/mailboom/api/domain/model/campaign/Campaign.java
+++ api/src/main/java/com/mailboom/api/domain/model/campaign/Campaign.java
@@ -1,6 +1,5 @@
 package com.mailboom.api.domain.model.campaign;
 
-
 import com.mailboom.api.domain.model.campaign.valueobjects.*;
 import com.mailboom.api.domain.model.contact.valueobjects.ContactListId;
 import com.mailboom.api.domain.model.user.valueobjects.UserId;
@@ -13,15 +12,15 @@ public class Campaign {
 
     private final CampaignId id;
     private final UserId owner;
-    private Subject subject;
-    private HtmlContent htmlContent;
-    private EmailSenderIdentity senderIdentity;
-    private ContactListId recipientList;
-    private CampaignStatus status;
+    private final Subject subject;
+    private final HtmlContent htmlContent;
+    private final EmailSenderIdentity senderIdentity;
+    private final ContactListId recipientList;
+    private final CampaignStatus status;
     private final Instant createdAt;
-    private Instant sentAt;
+    private final Instant sentAt;
 
-    private Campaign(CampaignId id, UserId owner, Subject subject, HtmlContent htmlContent,  EmailSenderIdentity senderIdentity, ContactListId recipientList, CampaignStatus status, Instant createdAt, Instant sentAt) {
+    private Campaign(CampaignId id, UserId owner, Subject subject, HtmlContent htmlContent, EmailSenderIdentity senderIdentity, ContactListId recipientList, CampaignStatus status, Instant createdAt, Instant sentAt) {
         this.id = id;
         this.owner = owner;
         this.subject = subject;
@@ -47,28 +46,29 @@ public class Campaign {
         );
     }
 
+    public static Campaign recreate(CampaignId id, UserId owner, Subject subject, HtmlContent htmlContent, EmailSenderIdentity sender, ContactListId recipientList, CampaignStatus status, Instant createdAt, Instant sentAt) {
+        return new Campaign(id, owner, subject, htmlContent, sender, recipientList, status, createdAt, sentAt);
+    }
+
     public void update(Subject subject, HtmlContent htmlContent, ContactListId recipientList) {
         if (this.status != CampaignStatus.DRAFT) {
             throw new IllegalStateException("You can only update a draft campaign.");
         }
-        this.subject = subject;
-        this.htmlContent = htmlContent;
-        this.recipientList = recipientList;
+        new Campaign(this.id, this.owner, subject, htmlContent, this.senderIdentity, recipientList, this.status, this.createdAt, this.sentAt);
     }
 
     public void markAsSending() {
         if (this.status != CampaignStatus.DRAFT) {
             throw new IllegalStateException("Campaign is not in a draft state.");
         }
-        this.status = CampaignStatus.SENDING;
+        new Campaign(this.id, this.owner, this.subject, this.htmlContent, this.senderIdentity, this.recipientList, CampaignStatus.SENDING, this.createdAt, null);
     }
 
     public void markAsSent() {
         if (this.status != CampaignStatus.SENDING) {
             throw new IllegalStateException("Campaign is not in a sending state.");
         }
-        this.status = CampaignStatus.SENT;
-        this.sentAt = Instant.now();
+        new Campaign(this.id, this.owner, this.subject, this.htmlContent, this.senderIdentity, this.recipientList, CampaignStatus.SENT, this.createdAt, Instant.now());
     }
 
     public String getFullSenderName() {
