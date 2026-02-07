bc124238dba09936a16a04878642d33a579c882b
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-07 12:34:30 +0100

    feat(api): introduce Campaign entity integration in repository layer

---
+++ api/src/main/java/com/mailboom/api/infrastructure/campaign/persistence/jpa/repository/SpringDataCampaignRepository.java
@@ -0,0 +1,11 @@
+package com.mailboom.api.infrastructure.campaign.persistence.jpa.repository;
+
+import com.mailboom.api.infrastructure.campaign.persistence.jpa.entity.CampaignEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface SpringDataCampaignRepository extends JpaRepository<CampaignEntity, UUID> {
+    List<CampaignEntity> findAllByOwnerId(UUID ownerId);
+}
