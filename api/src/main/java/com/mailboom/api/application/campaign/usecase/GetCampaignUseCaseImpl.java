d7ffb2b8b8aa42ab987606f45ff4467eb90fac38
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-08 23:34:12 +0100

    feat: add GetCampaignuse case implementation

---
+++ api/src/main/java/com/mailboom/api/application/campaign/usecase/GetCampaignUseCaseImpl.java
@@ -0,0 +1,24 @@
+package com.mailboom.api.application.campaign.usecase;
+
+import com.mailboom.api.application.campaign.port.in.GetCampaignUseCase;
+import com.mailboom.api.application.campaign.usecase.command.GetCampaignCommand;
+import com.mailboom.api.domain.model.campaign.Campaign;
+import com.mailboom.api.domain.model.campaign.valueobjects.CampaignId;
+import com.mailboom.api.domain.port.out.CampaignRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class GetCampaignUseCaseImpl implements GetCampaignUseCase {
+    private final CampaignRepository campaignRepository;
+
+    @Override
+    public Campaign excecute(GetCampaignCommand command) {
+        if (command.id() == null || command.id().isBlank()) {
+            throw new IllegalArgumentException("Campaign ID is required");
+        }
+        CampaignId campaignId = CampaignId.fromString(command.id());
+        return campaignRepository.findById(campaignId);
+    }
+}
