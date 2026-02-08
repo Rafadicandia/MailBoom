78362dae4a08474861f2a851ec43d8560533c2a9
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-08 04:16:42 +0100

    feat(api): add delete and get sent campaigns endpoints to CampaignController

--- api/src/main/java/com/mailboom/api/infrastructure/campaign/controller/CampaignController.java
+++ api/src/main/java/com/mailboom/api/infrastructure/campaign/controller/CampaignController.java
@@ -1,17 +1,24 @@
 package com.mailboom.api.infrastructure.campaign.controller;
 
 import com.mailboom.api.application.campaign.port.in.CreateCampaignUseCase;
+import com.mailboom.api.application.campaign.port.in.DeleteCampaignUseCase;
+import com.mailboom.api.application.campaign.port.in.GetSentCampaignsFromUserUseCase;
 import com.mailboom.api.application.campaign.usecase.command.CreateCampaignCommand;
+import com.mailboom.api.application.campaign.usecase.command.DeleteCampaignCommand;
+import com.mailboom.api.application.campaign.usecase.command.GetSentCampaignsFromUserCommand;
 import com.mailboom.api.domain.model.campaign.Campaign;
+import com.mailboom.api.infrastructure.campaign.dto.CampaignDataResponse;
 import com.mailboom.api.infrastructure.campaign.dto.NewCampaignRequest;
 import com.mailboom.api.infrastructure.campaign.dto.NewCampaignResponse;
+import com.mailboom.api.infrastructure.security.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/campaigns")
@@ -19,6 +26,8 @@ import org.springframework.web.bind.annotation.RestController;
 @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 public class CampaignController {
     private final CreateCampaignUseCase createCampaignUseCase;
+    private final GetSentCampaignsFromUserUseCase getSentCampaignsFromUserUseCase;
+    private final DeleteCampaignUseCase deleteCampaignUseCase;
 
     @PostMapping("/new")
     @PreAuthorize("@userSecurity.isOwner(authentication, #request.ownerId())")
@@ -42,6 +51,32 @@ public class CampaignController {
                 newCampaign.getCreatedAt().toString()
         ));
 
+    }
 
+    @GetMapping("/user/{id}")
+    @PreAuthorize("@userSecurity.isOwner(authentication, #id)")
+    public ResponseEntity<List<CampaignDataResponse>> getSentCampaignsFromUser(@PathVariable UUID id) {
+        GetSentCampaignsFromUserCommand getSentCampaignsFromUserCommand = new GetSentCampaignsFromUserCommand(id.toString());
+        List<Campaign> sentCampaigns = getSentCampaignsFromUserUseCase.execute(getSentCampaignsFromUserCommand);
+        return ResponseEntity.ok(sentCampaigns
+                .stream()
+                .map(
+                        campaign -> new CampaignDataResponse(
+                                campaign.getId().value().toString(),
+                                campaign.getOwner().value().toString(),
+                                campaign.getSubject().value(),
+                                campaign.getHtmlContent().value(),
+                                campaign.getSenderIdentity().value(),
+                                campaign.getRecipientList().value().toString(),
+                                campaign.getStatus().name(),
+                                campaign.getCreatedAt().toString())).toList());
+    }
+
+    @DeleteMapping("/{id}/delete")
+    @PreAuthorize("@userSecurity.isCampaignOwner(authentication, #id)")
+    public ResponseEntity<Void> deleteCampaign(@PathVariable UUID id) {
+
+        deleteCampaignUseCase.excecute(new DeleteCampaignCommand(id.toString()));
+        return ResponseEntity.noContent().build();
     }
 }
