4374ad09d7b644635e73bace40b17f45b84214ad
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-09 10:38:01 +0100

    fix(api): improve campaign not found exception handling
    Replaced null check with Optional.orElseThrow for better exception handling when campaign is not found

--- api/src/main/java/com/mailboom/api/application/campaign/usecase/DeleteCampaignUseCaseImpl.java
+++ api/src/main/java/com/mailboom/api/application/campaign/usecase/DeleteCampaignUseCaseImpl.java
@@ -2,9 +2,7 @@ package com.mailboom.api.application.campaign.usecase;
 
 import com.mailboom.api.application.campaign.port.in.DeleteCampaignUseCase;
 import com.mailboom.api.application.campaign.usecase.command.DeleteCampaignCommand;
-import com.mailboom.api.application.common.exception.CampaignIsNotFromUserException;
 import com.mailboom.api.domain.model.campaign.valueobjects.CampaignId;
-import com.mailboom.api.domain.model.user.valueobjects.UserId;
 import com.mailboom.api.domain.port.out.CampaignRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -19,11 +17,8 @@ public class DeleteCampaignUseCaseImpl implements DeleteCampaignUseCase {
     @Override
     public void excecute(DeleteCampaignCommand command) {
         CampaignId campaignId = new CampaignId(UUID.fromString(command.campaignId()));
-        if (campaignRepository.findById(campaignId) == null) {
-            throw new IllegalArgumentException("Campaign with id " + command.campaignId() + " does not exist");
-
-        }
+        campaignRepository.findById(campaignId)
+                .orElseThrow(() -> new IllegalArgumentException("Campaign with id " + command.campaignId() + " does not exist"));
         campaignRepository.deleteById(campaignId);
-
     }
 }
