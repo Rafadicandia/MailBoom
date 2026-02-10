b8a0011523c302cb45abfd1602bc2eb68b9bddff
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-10 16:52:23 +0100

    feat: add proper implementation for emailSender interface. This class now handles the email content request formatted in the aws SES needs. It also implements a custom exception in case there is any issue during sending

---
+++ api/src/main/java/com/mailboom/api/infrastructure/campaign/awsadapter/AwsEmailSenderAdapter.java
@@ -0,0 +1,75 @@
+package com.mailboom.api.infrastructure.campaign.awsadapter;
+
+import com.mailboom.api.domain.model.campaign.Campaign;
+import com.mailboom.api.domain.model.common.valueobjects.Email;
+import com.mailboom.api.domain.model.contact.Contact;
+import com.mailboom.api.domain.model.user.User;
+import com.mailboom.api.domain.port.out.ContactRepository;
+import com.mailboom.api.domain.port.out.EmailSender;
+import com.mailboom.api.domain.port.out.UserRepository;
+import com.mailboom.api.infrastructure.common.exception.EmailSendingException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.sesv2.SesV2Client;
+import software.amazon.awssdk.services.sesv2.model.*;
+
+import java.util.List;
+
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class AwsEmailSenderAdapter implements EmailSender {
+    private final SesV2Client sesClient;
+    private final ContactRepository contactRepository;
+    private final UserRepository userRepository;
+
+    private static final int BATCH_SIZE = 50;
+
+
+    @Override
+    public void send(Campaign campaign) {
+        User owner = userRepository.findById(campaign.getOwner()).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        List<Contact> recipientList = contactRepository.findAllByContactListId(campaign.getRecipientList());
+        List<String> totalEmailsInList = recipientList.stream().map(Contact::getEmail).map(Email::toString).toList();
+
+        EmailContent emailContent = EmailContent.builder().simple(Message.builder()
+                        .subject(Content.builder().data(campaign.getSubject().value()).charset("UTF-8").build())
+                        .body(Body.builder()
+                                .html(Content.builder().data(campaign.getHtmlContent().value()).charset("UTF-8").build())
+                                .build())
+                        .build())
+                .build();
+
+        String identityArn = System.getenv("MAILBOOM_DOMAIN_ARN");
+
+
+
+        for (int i = 0; i < totalEmailsInList.size(); i += BATCH_SIZE) {
+            int endIndex = Math.min(i + BATCH_SIZE, totalEmailsInList.size());
+            List<String> batch = totalEmailsInList.subList(i, endIndex);
+
+            Destination destination = Destination.builder()
+                    .bccAddresses(batch).build();
+
+            SendEmailRequest emailRequest =
+                    SendEmailRequest.builder()
+                            .fromEmailAddress(campaign.getFullSenderName())
+                            .fromEmailAddressIdentityArn(identityArn)
+                            .destination(destination)
+                            .replyToAddresses(owner.getEmail().email())
+                            .feedbackForwardingEmailAddress(owner.getEmail().email())
+                            .feedbackForwardingEmailAddressIdentityArn(identityArn)
+                            .content(emailContent)
+                            .build();
+            try {
+                sesClient.sendEmail(emailRequest);
+
+            } catch (SesV2Exception e) {
+                log.error("Error sending email batch for campaign {}: {}", campaign.getId().value(), e.getMessage());
+                throw new EmailSendingException("Error sending email batch for campaign " + campaign.getId().value());
+            }
+         }
+    }
+}
