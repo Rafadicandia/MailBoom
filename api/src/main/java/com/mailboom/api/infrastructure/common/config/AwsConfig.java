c9b522d9e7e1d830bb561af35a96ccd1b60fd62e
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-11 12:25:57 +0100

    refactor(config): update AWS SES configuration to use static credentials and custom endpoint
    Replaces environment variable-based AWS region with Spring Cloud AWS configuration properties. Implements static credentials provider using access key and secret key from configuration. Adds support for custom SES endpoint override when specified in configuration.

--- api/src/main/java/com/mailboom/api/infrastructure/common/config/AwsConfig.java
+++ api/src/main/java/com/mailboom/api/infrastructure/common/config/AwsConfig.java
@@ -1,22 +1,45 @@
 package com.mailboom.api.infrastructure.common.config;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sesv2.SesV2Client;
+import software.amazon.awssdk.services.sesv2.SesV2ClientBuilder;
+
+import java.net.URI;
 
 @Configuration
 public class AwsConfig {
 
-    private final String awsRegion=System.getenv("AWS_REGION");
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${spring.cloud.aws.ses.endpoint:#{null}}")
+    private String endpoint;
 
     @Bean
-    public SesV2Client sesClient() {
-        return SesV2Client.builder()
-                .region(Region.of(awsRegion))
-                .credentialsProvider(DefaultCredentialsProvider.create())
-                .build();
+    public SesV2Client sesV2Client() {
+        AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
+            software.amazon.awssdk.auth.credentials.AwsBasicCredentials.create(accessKey, secretKey)
+        );
+
+        SesV2ClientBuilder builder = SesV2Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(credentialsProvider);
+
+        if (endpoint != null && !endpoint.isEmpty()) {
+            builder.endpointOverride(URI.create(endpoint));
+        }
+
+        return builder.build();
     }
 }
