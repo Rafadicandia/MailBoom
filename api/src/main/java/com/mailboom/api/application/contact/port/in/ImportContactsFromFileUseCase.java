e0dea6558d49156ed294ac0bbe1f76286c16643b
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-13 09:38:19 +0100

    refactor(api): use command object for ImportContactsFromFileUseCase

--- api/src/main/java/com/mailboom/api/application/contact/port/in/ImportContactsFromFileUseCase.java
+++ api/src/main/java/com/mailboom/api/application/contact/port/in/ImportContactsFromFileUseCase.java
@@ -1,10 +1,11 @@
 package com.mailboom.api.application.contact.port.in;
 
+import com.mailboom.api.application.contact.usecase.command.ImportContactsFromFileCommand;
 import com.mailboom.api.domain.model.contact.valueobjects.ContactListId;
 import com.mailboom.api.domain.model.user.valueobjects.UserId;
 
 import java.io.InputStream;
 
 public interface ImportContactsFromFileUseCase {
-    void execute(ContactListId listId, UserId ownerId, InputStream fileStream, String contentType);
+    void execute(ImportContactsFromFileCommand command);
 }
