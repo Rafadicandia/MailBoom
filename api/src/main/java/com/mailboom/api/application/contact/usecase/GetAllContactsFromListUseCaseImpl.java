18e511e79a8f616e4ba7c0879a7a513f042e810d
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-20 09:52:37 +0100

    feat(api): add GetAllContactsFromListUseCase implementation

---
+++ api/src/main/java/com/mailboom/api/application/contact/usecase/GetAllContactsFromListUseCaseImpl.java
@@ -0,0 +1,27 @@
+package com.mailboom.api.application.contact.usecase;
+
+import com.mailboom.api.application.contact.port.in.GetAllContactsFromListUseCase;
+import com.mailboom.api.application.contact.usecase.command.GetAllContactsFromListCommand;
+import com.mailboom.api.domain.model.contact.Contact;
+import com.mailboom.api.domain.model.contact.valueobjects.ContactListId;
+import com.mailboom.api.domain.port.out.ContactRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@AllArgsConstructor
+public class GetAllContactsFromListUseCaseImpl implements GetAllContactsFromListUseCase {
+    private final ContactRepository contactRepository;
+
+    @Override
+    public List<Contact> execute(GetAllContactsFromListCommand command) {
+        if (command.listId() == null || command.listId().isBlank()) {
+            throw new IllegalArgumentException("List ID is required");
+        }
+        ContactListId contactListId = new ContactListId(UUID.fromString(command.listId()));
+        return contactRepository.findAllByContactListId(contactListId);
+    }
+}
