c8eb18ae83efb23f87796df8b25df28e9bcd009c
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-20 09:49:51 +0100

    feat(api): update contact list total on contact delete

--- api/src/main/java/com/mailboom/api/application/contact/usecase/DeleteContactUseCaseImpl.java
+++ api/src/main/java/com/mailboom/api/application/contact/usecase/DeleteContactUseCaseImpl.java
@@ -2,22 +2,37 @@ package com.mailboom.api.application.contact.usecase;
 
 import com.mailboom.api.application.contact.port.in.DeleteContactUseCase;
 import com.mailboom.api.application.contact.usecase.command.DeleteContactCommand;
+import com.mailboom.api.domain.model.contact.Contact;
+import com.mailboom.api.domain.model.contact.ContactList;
 import com.mailboom.api.domain.model.contact.valueobjects.ContactId;
+import com.mailboom.api.domain.model.contact.valueobjects.ContactListId;
+import com.mailboom.api.domain.port.out.ContactListRepository;
 import com.mailboom.api.domain.port.out.ContactRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @AllArgsConstructor
 public class DeleteContactUseCaseImpl implements DeleteContactUseCase {
     private final ContactRepository contactRepository;
+    private final ContactListRepository contactListRepository;
+
 
     @Override
     public void execute(DeleteContactCommand command) {
+        Contact contact = contactRepository.findById(ContactId.fromString(command.id())).orElseThrow();
+        ContactListId contactListId = ContactListId.fromString(contact.getListId().toString());
+        ContactList contactList = contactListRepository.findById(contactListId).orElseThrow();
+
         if (contactRepository.findById(ContactId.fromString(command.id())).isEmpty()) {
             throw new IllegalArgumentException("Contact with id " + command.id() + " does not exist");
         }
         contactRepository.deleteById(ContactId.fromString(command.id()));
         System.out.println("Contact with id " + command.id() + " deleted successfully");
+        List<Contact> totalContacts = contactRepository.findAllByContactListId(contactListId);
+        ContactList updatedContactList = contactList.updateTotalContacts(totalContacts.size());
+        contactListRepository.save(updatedContactList);
     }
 }
