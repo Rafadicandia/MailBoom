01be9a4e2faae28c532a251afed0b58ce0ffc785
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-02-20 09:50:19 +0100

    refactor(api): enhance ErrorResponse with status and error fields

--- api/src/main/java/com/mailboom/api/infrastructure/common/dto/ErrorResponse.java
+++ api/src/main/java/com/mailboom/api/infrastructure/common/dto/ErrorResponse.java
@@ -2,5 +2,10 @@ package com.mailboom.api.infrastructure.common.dto;
 
 import java.time.LocalDateTime;
 
-public record ErrorResponse(String message, LocalDateTime timestamp) {
+public record ErrorResponse(
+        int status,
+        String error,
+        String message,
+        LocalDateTime timestamp
+) {
 }
