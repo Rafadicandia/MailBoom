490878e5c74ef6d24b961f5445e77ac9ae9bb6c6
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-03-07 12:08:08 +0100

    feat(api): add phone field to Contact model

--- api/src/main/java/com/mailboom/api/domain/model/contact/Contact.java
+++ api/src/main/java/com/mailboom/api/domain/model/contact/Contact.java
@@ -16,32 +16,34 @@ public class Contact {
     private final ContactListId listId;
     private final Email email;
     private final Name name;
+    private final Phone phone;
     private final Map<String, Object> customFields;
     private final boolean subscribed;
 
-    private Contact(ContactId id, ContactListId listId, Email email, Name name, Map<String, Object> customFields, boolean subscribed) {
+    private Contact(ContactId id, ContactListId listId, Email email, Name name, Phone phone, Map<String, Object> customFields, boolean subscribed) {
         this.id = id;
         this.listId = listId;
         this.email = email;
         this.name = name;
+        this.phone = phone;
         this.customFields = customFields;
         this.subscribed = subscribed;
     }
 
-    public static Contact create(ContactId id, ContactListId listId, Email email, Name name, Map<String, Object> customFields, boolean subscribed){
-        return new Contact(id, listId, email, name, customFields, subscribed);
+    public static Contact create(ContactId id, ContactListId listId, Email email, Name name, Phone phone, Map<String, Object> customFields, boolean subscribed){
+        return new Contact(id, listId, email, name, phone, customFields, subscribed);
     }
 
-    public Contact updateContact(Email email, Name name, Map<String, Object> customFields, boolean subscribed){
-        return new Contact(this.id, this.listId, email, name, customFields, subscribed);
+    public Contact updateContact(Email email, Name name, Phone phone, Map<String, Object> customFields, boolean subscribed){
+        return new Contact(this.id, this.listId, email, name, phone, customFields, subscribed);
     }
 
-    public Contact subscribe(Contact contact){
-        return Contact.create(id, listId, email, name, customFields, true);
+    public Contact subscribe(){
+        return new Contact(id, listId, email, name, phone, customFields, true);
     }
 
-    public Contact unSubscribe(Contact contact){
-        return Contact.create(id, listId, email, name, customFields, false);
+    public Contact unSubscribe(){
+        return new Contact(id, listId, email, name, phone, customFields, false);
     }
 
 
