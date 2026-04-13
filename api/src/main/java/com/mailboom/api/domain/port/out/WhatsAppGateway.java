dffeaab9934a98c6d85baeb79c7d03bf4a36796a
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-04-13 13:14:46 +0200

    feat(api): add ClientConfig parameter to sendTemplateForReview

--- api/src/main/java/com/mailboom/api/domain/port/out/WhatsAppGateway.java
+++ api/src/main/java/com/mailboom/api/domain/port/out/WhatsAppGateway.java
@@ -1,7 +1,8 @@
 package com.mailboom.api.domain.port.out;
 
+import com.mailboom.api.domain.model.whatsapp.ClientConfig;
 import com.mailboom.api.domain.model.whatsapp.Template;
 
 public interface WhatsAppGateway {
-    void sendTemplateForReview(Template template);
+    void sendTemplateForReview(Template template,  ClientConfig config);
 }
