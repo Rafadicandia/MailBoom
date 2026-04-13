0b8a751eb0646337ef22c5d39c11c9fd0fa918a6
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-04-13 12:36:02 +0200

    refactor(api): use UserId value object for clientId

--- api/src/main/java/com/mailboom/api/domain/model/whatsapp/ClientConfig.java
+++ api/src/main/java/com/mailboom/api/domain/model/whatsapp/ClientConfig.java
@@ -1,17 +1,18 @@
 package com.mailboom.api.domain.model.whatsapp;
 
+import com.mailboom.api.domain.model.user.valueobjects.UserId;
 import com.mailboom.api.domain.model.whatsapp.valueobjects.AccessToken;
 import lombok.Getter;
 
 import java.util.UUID;
 @Getter
 public class ClientConfig {
-    private final UUID clientId;
+    private final UserId clientId;
     private final String wabaId;
     private final String phoneNumberId;
     private final AccessToken accessToken;
 
-    public ClientConfig(UUID clientId, String wabaId, String phoneNumberId, AccessToken accessToken) {
+    public ClientConfig(UserId clientId, String wabaId, String phoneNumberId, AccessToken accessToken) {
         this.clientId = clientId;
         this.wabaId = wabaId;
         this.phoneNumberId = phoneNumberId;
