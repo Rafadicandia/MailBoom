f9954fc08163d61573a50e8e29069a316cb51b58
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-04-19 18:13:28 +0200

    fix(api): change accessToken storage to plain text

--- api/src/main/java/com/mailboom/api/infrastructure/whatsapp/persistence/jpa/entity/ClientConfigEntity.java
+++ api/src/main/java/com/mailboom/api/infrastructure/whatsapp/persistence/jpa/entity/ClientConfigEntity.java
@@ -24,6 +24,7 @@ public class ClientConfigEntity {
     @Column(nullable = false)
     private String phoneNumberId;
     
-    @Convert(converter = EncryptionConverter.class)
+    //@Convert(converter = EncryptionConverter.class)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String accessToken;
 }
