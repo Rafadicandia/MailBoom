244758868e33745460752af7add777c84a835ea0
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-04-19 18:34:33 +0200

    feat(api): add sendTemplateMessage method to WhatsAppGateway

--- api/src/main/java/com/mailboom/api/domain/port/out/WhatsAppGateway.java
+++ api/src/main/java/com/mailboom/api/domain/port/out/WhatsAppGateway.java
@@ -2,7 +2,12 @@ package com.mailboom.api.domain.port.out;
 
 import com.mailboom.api.domain.model.whatsapp.ClientConfig;
 import com.mailboom.api.domain.model.whatsapp.Template;
+import com.mailboom.api.domain.model.common.valueobjects.Phone;
+
+import java.util.List;
 
 public interface WhatsAppGateway {
-    void sendTemplateForReview(Template template,  ClientConfig config);
+    void sendTemplateForReview(Template template, ClientConfig config);
+
+    void sendTemplateMessage(Phone to, Template template, List<String> parameters, ClientConfig config);
 }
