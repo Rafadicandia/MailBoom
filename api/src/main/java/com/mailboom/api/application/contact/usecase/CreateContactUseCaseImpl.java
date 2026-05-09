f710f55b2a6c0a5d424e26934412f85242b578de
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-05-09 19:47:21 +0200

    refactor(api): comment out contact list total contacts update in CreateContactUseCase
    Comment out contact list fetch and total contacts increment logic during
    contact creation. Added TODO to refactor how total contacts are displayed
    to the frontend before re-enabling the update logic.

--- api/src/main/java/com/mailboom/api/application/contact/usecase/CreateContactUseCaseImpl.java
+++ api/src/main/java/com/mailboom/api/application/contact/usecase/CreateContactUseCaseImpl.java
@@ -2,25 +2,23 @@ package com.mailboom.api.application.contact.usecase;
 
 import com.mailboom.api.application.contact.port.in.CreateContactUseCase;
 import com.mailboom.api.application.contact.usecase.command.CreateContactCommand;
-import com.mailboom.api.domain.model.contact.Contact;
-import com.mailboom.api.domain.model.contact.ContactList;
-import com.mailboom.api.domain.model.contact.valueobjects.ContactId;
-import com.mailboom.api.domain.model.contact.valueobjects.ContactListId;
 import com.mailboom.api.domain.model.common.valueobjects.Email;
 import com.mailboom.api.domain.model.common.valueobjects.Name;
 import com.mailboom.api.domain.model.common.valueobjects.Phone;
+import com.mailboom.api.domain.model.contact.Contact;
+import com.mailboom.api.domain.model.contact.valueobjects.ContactId;
+import com.mailboom.api.domain.model.contact.valueobjects.ContactListId;
 import com.mailboom.api.domain.port.out.ContactListRepository;
 import com.mailboom.api.domain.port.out.ContactRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @AllArgsConstructor
 public class CreateContactUseCaseImpl implements CreateContactUseCase {
     private final ContactRepository contactRepository;
     private final ContactListRepository contactListRepository;
+
 
     @Override
     public Contact execute(CreateContactCommand command) {
@@ -29,8 +27,9 @@ public class CreateContactUseCaseImpl implements CreateContactUseCase {
         }
 
         ContactListId contactListId = ContactListId.fromString(command.contactListId());
-        ContactList contactList = contactListRepository.findById(contactListId).orElseThrow();
-        List<Contact> totalContacts = contactRepository.findAllByContactListId(contactListId);
+        //ContactList contactList = contactListRepository.findById(contactListId).orElseThrow();
+        //List<Contact> totalContacts = contactRepository.findAllByContactListId(contactListId);
+
 
         Contact newContact = Contact.create(
                 ContactId.generate(),
@@ -41,8 +40,10 @@ public class CreateContactUseCaseImpl implements CreateContactUseCase {
                 command.customFields(),
                 command.subscribed()
         );
-        ContactList updatedContactList = contactList.updateTotalContacts(totalContacts.size() + 1);
-        contactListRepository.save(updatedContactList);
+
+        //TODO: I need to refactor how total contacts are displayed to frontend.
+        //ContactList updatedContactList = contactList.updateTotalContacts(totalContacts.size() + 1);
+        //contactListRepository.save(updatedContactList);
         return contactRepository.save(newContact);
 
 
